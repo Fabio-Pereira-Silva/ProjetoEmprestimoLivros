06197a1b1f4d9995ad0552fbeed6fc82f1945dc4
Fabio Pereira da Silva <fabio.silva@interplayers.com.br>
2025-02-04 14:12:41 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+
+    }
+}
